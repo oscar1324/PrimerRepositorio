9df27356f07113a3aad46bf435578a0b806bc3d5
Portatil <Portatil@PORTATIL>
2020-06-17 07:31:49 +0200

    Inicializar rama master

---
+++ recuperacion3aEva/src/com/java/Inicio.java
@@ -0,0 +1,20 @@
+package com.java;
+
+/**
+ * Esta clase es una clase de prueba. No debes incluir ningún cambio en ella.
+ * Si sigues los pasos indicados en el enunciado, esta clase debería desaparecer en tu rama.
+ * @author Portatil
+ *
+ */
+public class Inicio {
+
+	public Inicio() {
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
