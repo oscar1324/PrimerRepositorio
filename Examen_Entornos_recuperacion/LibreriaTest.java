ae001ec762142c9595104a64c5efa6b2ababeb5e
Usuario <Usuario@DESKTOP-UGSENBH>
2020-06-18 13:33:35 +0200

    Test en el caso de pruebas

--- Examen_Entornos_recuperacion/LibreriaTest.java
+++ Examen_Entornos_recuperacion/LibreriaTest.java
@@ -1,12 +1,43 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class LibreriaTest {
+	
+	private static Libreria libreriaa;
+	
+	@BeforeAll
+	static void inicio() {
+		libreriaa = new Libreria();
+	}
 
 	@Test
-	void test() {
-		fail("No implementado aun");
+	void testNuevoLibro() throws Exception{
+		String resultado = libreriaa.nuevoLibro();
+		assertNull(resultado);
+		
 	}
+	
+	@Test
+	void testdescatalogarLibro()throws Exception {
+		String resultado = libreriaa.descatalogarLibro();
+		assertNotNull(resultado);
+	}
+	
+	@Test
+	void testeliminarEjemplarLibro() throws Exception{
+		String resultado = libreriia.eliminarEjemplarLibro();
+		assertEquals("0", resultado);
+		assertNull(resultado);
+		
+	}
+	
+	@Test
+	void testrecuperarInfoLibro() throws Exception{
+		String resultado = libreriia.recuperarInfoLibro();
+		
+	}
+	
 
 }
