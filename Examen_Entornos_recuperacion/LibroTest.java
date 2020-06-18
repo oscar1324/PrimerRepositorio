73c426d346729d995014355e8bb2003b56dd42aa
Usuario <Usuario@DESKTOP-UGSENBH>
2020-06-18 13:46:16 +0200

    Casos de prueba

--- Examen_Entornos_recuperacion/LibroTest.java
+++ Examen_Entornos_recuperacion/LibroTest.java
@@ -1,12 +1,37 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class LibroTest {
+	private static Libro x;
 
+	@BeforeAll
+	static void inicio() {
+		x = new Libro();
+	}
+	
 	@Test
-	void test() {
-		fail("No implementado aun");
+	void testincrementarNumEjemplares() {
+		String resultado = x.incrementarNumEjemplares();
+		assertEquals("", resultado);
+		assertNotNull(resultado);
+		assertThrows(IllegalArgumentException.class, () -> {new incrementarNumEjemplares(null);});
+	}
+	
+	@Test
+	void testdecrementarNumEjemplares() {
+		String resultado = x.decrementarNumEjemplares();
+		assertNotNull(resultado);
+		assertThrows(IllegalArgumentException.class, () -> {new decrementarNumEjemplares(null);});
+	}
+	
+	@Test
+	void testrecuperarCantidadEjemplares() {
+		String resultado = x.recuperarCantidadEjemplares();
+		assertEquals("", resultado);
+		assertNotNull(resultado);
+		assertThrows(Exception.class, () -> {new recuperarCantidadEjemplares(null);});
 	}
 
 }
